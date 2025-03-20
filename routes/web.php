4e5c364d1da173b6b7b2c08d23aff568df4eace6
coderman007 <coderman1980@gmail.com>
2025-03-19 22:50:53 -0500

    Configuración básica de Websockets OK

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Events\MyEvent;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/proof', function () {
+    event(new MyEvent);
+    return 'Event has been sent!';
 });
